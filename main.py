dcb30a2d93ee7ac1a576e9e34a9230e012d55d39
ck <hungchunka.ck@gmail.com>
2021-08-14 13:56:21 +0900

    - Set max_instances - Get secret from secret manager

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 # [START gae_python39_app]
 # [START gae_python3_app]
 import os
-import json
 from datetime import datetime
 from flask import Flask
 from google.cloud import secretmanager
@@ -27,9 +26,25 @@ def helloName(name = 'World'):
 def health():
     return 'I am healty!'
 
-@app.route('/env')
-def env():
-    return dict(os.environ)
+
+@app.route('/secret')
+def secret():
+    # Create the Secret Manager client.
+    client = secretmanager.SecretManagerServiceClient()
+
+    project_id = os.getenv('GOOGLE_CLOUD_PROJECT')
+    # Build the parent name from the project.
+    name = f"projects/{project_id}/secrets/test/versions/latest"
+
+    # Access the secret version.
+    response = client.access_secret_version(request={"name": name})
+
+    # Print the secret payload.
+    #
+    # WARNING: Do not print the secret in a production environment - this
+    # snippet is showing how to access the secret material.
+    payload = response.payload.data.decode("UTF-8")
+    return str(payload)
 
 if __name__ == '__main__':
     # This is used when running locally only. When deploying to Google App
